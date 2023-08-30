191dabac330bb1fd938ee73c3b6889de7c3f0c07
kevinyun31 <kevinyun31@gmail.com>
2023-08-30 19:09:51 +0900

    게시글 문의 작성

--- src/main/java/shop/mtcoding/blogv2/board/BoardService.java
+++ src/main/java/shop/mtcoding/blogv2/board/BoardService.java
@@ -1,8 +1,27 @@
 package shop.mtcoding.blogv2.board;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import shop.mtcoding.blogv2.user.User;
 
 @Service
 public class BoardService {
 
+      @Autowired
+    private BoardRepository boardRepository;
+
+    @Transactional
+    public void 글쓰기(BoardRequest.SaveDTO saveDTO, int sessionUserId) {
+        // Board 객체 생성 및 데이터 설정
+        Board board = Board.builder()
+                .title(saveDTO.getTitle())
+                .content(saveDTO.getContent())
+                .user(User.builder().id(sessionUserId).build()) // 게시글 작성자 설정
+                .build();
+
+        // 생성한 Board 객체를 Repository를 통해 데이터베이스에 저장
+        boardRepository.save(board);
+    }
 }
