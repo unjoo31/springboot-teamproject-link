8ac450f0711e340b192d7f7afaa62a6edbded7e5
unyak <0106un@naver.com>
2023-08-31 15:34:15 +0900

    채용공고 현황 화면 구연/마감일 안함

--- src/main/java/shop/mtcoding/blogv2/notice/NoticeController.java
+++ src/main/java/shop/mtcoding/blogv2/notice/NoticeController.java
@@ -1,8 +1,30 @@
 package shop.mtcoding.blogv2.notice;
 
+import javax.servlet.http.HttpServletRequest;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import shop.mtcoding.blogv2.skill.Skill;
 
 @Controller
 public class NoticeController {
+
+    @Autowired
+    private NoticeService noticeService;
     
+
+    // String
+    @GetMapping("/corporationSupport")
+    public String corporationSupport(HttpServletRequest request){
+
+        Notice notice = noticeService.공고목록보기(1);
+        System.out.println("테스트 : " + notice.getTitle());
+        System.out.println("테스트 : " + notice.getHashSkilList().get(0).toString());
+        System.out.println("테스트 : " + notice.getUser().getName());
+        request.setAttribute("notice", notice);
+        return "/corporation/corporationSupport";
+    }
 }
--- src/main/java/shop/mtcoding/blogv2/notice/NoticeService.java
+++ src/main/java/shop/mtcoding/blogv2/notice/NoticeService.java
@@ -1,8 +1,16 @@
 package shop.mtcoding.blogv2.notice;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class NoticeService {
+
+    @Autowired
+    private NoticeRepository noticeRepository;
+
+    public Notice 공고목록보기(int s) {
+        return noticeRepository.findById(s).get();
+    }
     
 }
