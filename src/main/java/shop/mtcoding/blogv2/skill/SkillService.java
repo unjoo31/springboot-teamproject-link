dc9c2d6fa40e1d6d028672b2f22a581462fc826a
wanni29 <108277940+wanni29@users.noreply.github.com>
2023-09-01 11:43:24 +0900

    이력서 등록 완료
    [정완] 이력서 등록 완료

--- src/main/java/shop/mtcoding/blogv2/skill/SkillService.java
+++ src/main/java/shop/mtcoding/blogv2/skill/SkillService.java
@@ -1,8 +1,24 @@
 package shop.mtcoding.blogv2.skill;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class SkillService {
+
+    @Autowired
+    private SkillRepository skillRepository;
+
+
+    public List<Skill> 모든스킬가져오기() {
+
+        List<Skill> skill = skillRepository.findAll();
+        
+        return skill;
+    }
+
+
     
 }
