1b665b71a58ce8b6c2311cf4d5fe0aa41f3eb4a9
wanni29 <108277940+wanni29@users.noreply.github.com>
2023-09-04 09:25:27 +0900

    이력서 수정하기 구현완료
    [정완] 이력서 수정하기 구현완료

--- src/main/java/shop/mtcoding/blogv2/board/Board.java
+++ src/main/java/shop/mtcoding/blogv2/board/Board.java
@@ -44,7 +44,7 @@ public class Board {
     private String content;
 
     @JsonIgnore
-    @ManyToOne(fetch = FetchType.LAZY)
+    @ManyToOne(fetch = FetchType.LAZY, cascade = CascadeType.ALL)
     private User user;
 
     @JsonIgnoreProperties({"board"})
--- src/main/java/shop/mtcoding/blogv2/resume/ResumeRequest.java
+++ src/main/java/shop/mtcoding/blogv2/resume/ResumeRequest.java
@@ -25,8 +25,29 @@ public class ResumeRequest {
         private Integer career;
         private String content;
 
-        // Resume 에서 뽑아낼 데이터 [배열 타입으로]
-        // -> OneToMany관계에선 DTO를 안받아도 된다.
+        // Resume 에서 뽑아낼 데이터 [배열 타입으로
+        private ArrayList<String> skilList;
+        private ArrayList<String> areaList;
+
+    }
+
+
+    @Getter
+    @Setter
+    public static class ResumeUpdateDTO {
+
+                // User 에서 뽑아낼 데이터
+        private String name;
+        private String email;
+        private String address;
+        private String phoneNumber;
+        private Date date;
+
+        // Resume 에서 뽑아낼 데이터
+        private Integer career;
+        private String content;
+
+        // Resume 에서 뽑아낼 데이터 [배열 타입으로
         private ArrayList<String> skilList;
         private ArrayList<String> areaList;
 
