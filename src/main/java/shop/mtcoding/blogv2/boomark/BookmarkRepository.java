f66b1b0c34a2d9a0f24f3ed615908926c8461757
unjoo31 <unjoo31@gmail.com>
2023-09-04 19:02:00 +0900

    머지전저장

--- src/main/java/shop/mtcoding/blogv2/boomark/BookmarkRepository.java
+++ src/main/java/shop/mtcoding/blogv2/boomark/BookmarkRepository.java
@@ -1,7 +1,17 @@
 package shop.mtcoding.blogv2.boomark;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import shop.mtcoding.blogv2.user.User;
 
 public interface BookmarkRepository extends JpaRepository <Bookmark, Integer>{
+
+    @Query(value = "SELECT * FROM bookmark_tb t\n" + //
+            "LEFT JOIN user_tb u ON t.target_id = u.id\n" + //
+            "WHERE t.user_id IN :id", nativeQuery = true)
+    List<User> findByBookmark(Integer id);
     
 }
--- src/main/java/shop/mtcoding/blogv2/boomark/BookmarkService.java
+++ src/main/java/shop/mtcoding/blogv2/boomark/BookmarkService.java
@@ -1,8 +1,28 @@
 package shop.mtcoding.blogv2.boomark;
 
+import java.util.List;
+
+import javax.transaction.Transactional;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import shop.mtcoding.blogv2.notice.Notice;
+import shop.mtcoding.blogv2.user.User;
+
 
 @Service
 public class BookmarkService {
-    
+
+    @Autowired
+    private BookmarkRepository bookmarkRepository;
+
+    @Transactional
+    public void 북마크하기(Bookmark bookmark, Integer integer) {
+        bookmarkRepository.save(bookmark);
+    }
+
+    public List<User> 북마크기업찾기(Integer id) {
+        return bookmarkRepository.findByBookmark(id);
+    } 
 }
