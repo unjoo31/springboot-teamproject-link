f5d84674321d10619ecf675b21f8313b27442884
wanni29 <108277940+wanni29@users.noreply.github.com>
2023-09-06 14:28:53 +0900

    Topic/book mark seeker companies (#51)
    * 이력서 삭제하기
    
    * 합치기
    
    * 북마크[구직자입장에서 관심기업 기능구현완료]

--- src/main/java/shop/mtcoding/blogv2/hashskil/hashSkilRepository.java
+++ src/main/java/shop/mtcoding/blogv2/hashskil/hashSkilRepository.java
@@ -9,23 +9,25 @@ import org.springframework.data.repository.query.Param;
 
 import shop.mtcoding.blogv2.skill.Skill;
 
-public interface HashSkilRepository extends JpaRepository<HashSkil, Integer>{
-   
- 
-@Query("select h.skill FROM HashSkil h WHERE h.resume.id = :resumeId")
-List<Skill> findSkillsByResumeId(@Param("resumeId") Integer resumeId);
+public interface HashSkilRepository extends JpaRepository<HashSkil, Integer> {
 
+    @Query("select h.skill FROM HashSkil h WHERE h.resume.id = :resumeId")
+    List<Skill> findSkillsByResumeId(@Param("resumeId") Integer resumeId);
 
-@Query("select h.skill FROM HashSkil h WHERE h.notice.id = :noticeId")
-List<Skill> findSkillsByNoticeId(@Param("noticeId") Integer noticeId);
+    @Query("select h.skill FROM HashSkil h WHERE h.notice.id = :noticeId")
+    List<Skill> findSkillsByNoticeId(@Param("noticeId") Integer noticeId);
 
+    // 디비에 데이터가 쌓이는것을 방지하기위해 초기화를 위한 딜리트 메서드 생성
+    @Modifying
+    @Query("DELETE FROM HashSkil h WHERE h.resume.id = :resumeId")
+    void deleteByResumeId(@Param("resumeId") Integer resumeId);
 
-// 디비에 데이터가 쌓이는것을 방지하기위해 초기화를 위한 딜리트 메서드 생성
-@Modifying
-@Query("DELETE FROM HashSkil h WHERE h.resume.id = :resumeId")
-void deleteByResumeId(@Param("resumeId") Integer resumeId);
+    void deleteByNoticeId(Integer id);
 
+    @Query("SELECT h FROM HashSkil h INNER JOIN h.skill s INNER JOIN h.notice notice WHERE s.skillName = :skillName")
+    List<HashSkil> mfindSkillsBySkillName(@Param("skillName") String selectedSkills);
 
-void deleteByNoticeId(Integer id);
+    @Query("SELECT h FROM HashSkil h INNER JOIN h.skill s INNER JOIN h.resume resume WHERE s.skillName = :skillName")
+    List<HashSkil> mfindResumeBySkillName(@Param("skillName") String selectedSkills);
 
 }
