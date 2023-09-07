b35793da35bb094068acc0cc75ed82db515f8be2
yakyakyak12 <135561652+yakyakyak12@users.noreply.github.com>
2023-09-07 09:22:10 +0900

    회원정보 수정 완료 알림
    회원정보 수정 완료 알림

--- src/main/java/shop/mtcoding/blogv2/Blogv2Application.java
+++ src/main/java/shop/mtcoding/blogv2/Blogv2Application.java
@@ -7,9 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class Blogv2Application {
 
 	public static void main(String[] args) {
-		SpringApplication.run(Blogv2Application.class, args);
-
-		
+		SpringApplication.run(Blogv2Application.class, args);		
 	}
 
 }
